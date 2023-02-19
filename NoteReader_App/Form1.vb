28ec9b5b9b66751e21674171a1730c950d4b6c64
mesxx <ichsanfauzan2@gmail.com>
2023-02-19 20:04:03 +0700

    Add project files.

---
+++ NoteReader_App/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO.File
+Public Class Form1
+    Function TextFile()
+        Dim text As String = ""
+        If Exists("D:\text.txt") Then
+            text = ReadAllText("D:\text.txt")
+        End If
+        Return text
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim count As Integer = 0
+        For Each word As String In Split(TextFile(), " ")
+            count += 1
+        Next
+        MsgBox(count.ToString + " Words")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox(TextFile())
+    End Sub
+End Class
---
+++ NoteReader_App/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("NoteReader_App")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("NoteReader_App")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("103643aa-1f3b-43cf-9c4e-e4de91f9930c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
